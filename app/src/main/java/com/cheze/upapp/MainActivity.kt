50a96bc1466c31c1fcb40e9e03e457a11f3b84f3
chezecz <cheze92@gmail.com>
2020-10-16 14:55:04 +1100

    initialise Volley service to use HTTP resources

--- app/src/main/java/com/cheze/upapp/MainActivity.kt
+++ app/src/main/java/com/cheze/upapp/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.cheze.upapp.adapter.ItemAdapter
 import com.cheze.upapp.data.DataSource
+import com.cheze.upapp.service.VolleyService
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val accounts = DataSource(this).loadAccounts()
+        VolleyService.initialize(this)
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val accounts = DataSource(this).loadAccounts()
         recyclerView.adapter = ItemAdapter(this, accounts)
         recyclerView.setHasFixedSize(true)
     }
