4b00f4842c6a963d5375acb1b9d4b141d24c84f5
chezecz <cheze92@gmail.com>
2020-10-16 11:00:25 +1100

    update MainActivity.kt

--- app/src/main/java/com/cheze/upapp/MainActivity.kt
+++ app/src/main/java/com/cheze/upapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.cheze.upapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.cheze.upapp.adapter.ItemAdapter
+import com.cheze.upapp.data.DataSource
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val accounts = DataSource(this).loadAccounts()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, accounts)
+        recyclerView.setHasFixedSize(true)
     }
 }
