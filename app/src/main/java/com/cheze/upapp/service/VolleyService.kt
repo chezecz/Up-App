17dbcb3118f66722d3db817e2927c72dd81778a1
chezecz <cheze92@gmail.com>
2020-10-16 14:12:33 +1100

    add VolleyService.kt to communicate via HTTP

---
+++ app/src/main/java/com/cheze/upapp/service/VolleyService.kt
@@ -0,0 +1,16 @@
+package com.cheze.upapp.service
+
+import android.content.Context
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.Volley
+
+
+object VolleyService {
+    private lateinit var context: Context
+
+    val requestQueue: RequestQueue by lazy { Volley.newRequestQueue(context) }
+
+    fun initialize(context: Context) {
+        this.context = context.applicationContext
+    }
+}
