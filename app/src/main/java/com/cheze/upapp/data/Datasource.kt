035c5d2e5eaa26ba9bef8eba637d234ac832e199
chezecz <cheze92@gmail.com>
2020-10-19 11:05:37 +1100

    use the RequestWithHeaders class for retrieving data from API

--- app/src/main/java/com/cheze/upapp/data/Datasource.kt
+++ app/src/main/java/com/cheze/upapp/data/Datasource.kt
@@ -1,24 +1,22 @@
 package com.cheze.upapp.data
 
 import android.content.Context
-import android.widget.Toast
-import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import com.cheze.upapp.R
 import com.cheze.upapp.model.AccAttr
 import com.cheze.upapp.model.Account
 import com.cheze.upapp.model.Balance
-import com.cheze.upapp.service.VolleyService
-import org.json.JSONArray
+import com.cheze.upapp.service.RequestWithHeaders
 import org.json.JSONObject
-
-private const val TAG = "MyActivity"
+import kotlin.coroutines.suspendCoroutine
 
 class DataSource(private val context: Context) {
     private val secretValue = context.getString(R.string.up_api_key)
     private val baseUrl = "https://api.up.com.au/api/v1"
 
-    private fun convertJsonToObject(jsonArray: JSONArray): MutableList<Account> {
+    fun convertJsonToObject(jsonObject: JSONObject): MutableList<Account> {
         val listObjects = mutableListOf<Account>()
+        val jsonArray = jsonObject.getJSONArray("data")
         for (i in 0 until jsonArray.length()) {
             val jsonAccount: JSONObject = jsonArray.getJSONObject(i)
             val jsonAccountAttr: JSONObject = jsonAccount.getJSONObject("attributes")
@@ -41,34 +39,41 @@ class DataSource(private val context: Context) {
         return listObjects
     }
 
-    fun loadAccounts(): MutableList<Account> {
+    suspend fun loadAccounts() = suspendCoroutine<JSONObject> { cont ->
 
         val accountsUrl = "$baseUrl/accounts"
 
-        var listAccounts: MutableList<Account> = mutableListOf<Account>()
+//        val listAccounts: MutableList<Account> = mutableListOf<Account>()
 
-        val request = object: JsonObjectRequest(
-            Method.GET, accountsUrl, null,
-            { response ->
-                val accounts = response
-                    .getJSONArray("data")
-                listAccounts = convertJsonToObject(accounts)
-            },
-            {
-                Toast.makeText(context, "That didn't work!", Toast.LENGTH_SHORT).show()
-            }
-        )
-        {
-            override fun getHeaders(): MutableMap<String, String> {
-                val headers = HashMap<String, String>()
-                headers["Authorization"] = secretValue
-                return headers
-            }
+        val queue = Volley.newRequestQueue(this.context)
+
+        RequestWithHeaders(accountsUrl, cont, secretValue).also {
+            it.setShouldCache(false)
+            queue.add(it)
         }
 
-        VolleyService.requestQueue.add(request)
-        VolleyService.requestQueue.start()
+//        val request = object: JsonObjectRequest(
+//            Method.GET, accountsUrl, null,
+//            { response ->
+//                cont.resume(response)
+////                val accounts = response
+////                    .getJSONArray("data")
+////                listAccounts = convertJsonToObject(accounts)
+//            },
+//            {
+////                Toast.makeText(context, "That didn't work!", Toast.LENGTH_SHORT).show()
+//                cont.resume(null)
+//            }
+//        )
+//        {
+//            override fun getHeaders(): MutableMap<String, String> {
+//                val headers = HashMap<String, String>()
+//                headers["Authorization"] = secretValue
+//                return headers
+//            }
+//        }
 
-        return listAccounts
+//        VolleyService.requestQueue.add(request)
+//        VolleyService.requestQueue.start()
     }
 }
