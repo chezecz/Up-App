d4c12858ba450f2ffadbadb46835d541b2f05832
chezecz <cheze92@gmail.com>
2020-10-19 11:04:31 +1100

    use coroutines to work with Http requests

--- app/src/main/java/com/cheze/upapp/MainActivity.kt
+++ app/src/main/java/com/cheze/upapp/MainActivity.kt
@@ -6,15 +6,35 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cheze.upapp.adapter.ItemAdapter
 import com.cheze.upapp.data.DataSource
 import com.cheze.upapp.service.VolleyService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private lateinit var job: Job
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         VolleyService.initialize(this)
+        val context = applicationContext
+        job = Job()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val accounts = DataSource(this).loadAccounts()
-        recyclerView.adapter = ItemAdapter(this, accounts)
-        recyclerView.setHasFixedSize(true)
+        val accountRequest = DataSource(this)
+        launch {
+            val accounts = accountRequest.loadAccounts()
+            val accountsObject = accountRequest.convertJsonToObject(accounts)
+            recyclerView.adapter = ItemAdapter(context, accountsObject)
+            recyclerView.setHasFixedSize(true)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        job.cancel()
     }
 }
