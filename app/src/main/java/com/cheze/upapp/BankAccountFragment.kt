f140c96c6c695623e75d92845c317942d2b3a4fd
chezecz <cheze92@gmail.com>
2020-10-21 14:25:22 +1100

    redo RecyclerView activity as fragment

---
+++ app/src/main/java/com/cheze/upapp/BankAccountFragment.kt
@@ -0,0 +1,64 @@
+package com.cheze.upapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cheze.upapp.model.Account
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+/**
+ * A fragment representing a list of Items.
+ */
+class BankAccountFragment() : Fragment() {
+
+    private var columnCount = 1
+
+    private var data = listOf<Account>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            data = Json.decodeFromString<List<Account>>(it.getSerializable(ARG_DATA).toString())
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_item_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = MyBankAccountRecyclerViewAdapter(data)
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+        private const val ARG_DATA = "data"
+
+        @JvmStatic
+        fun newInstance(accounts: List<Account>) =
+            BankAccountFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(ARG_DATA,  Json.encodeToString(accounts))
+                }
+            }
+    }
+}
---
+++ app/src/main/java/com/cheze/upapp/MyBankAccountRecyclerViewAdapter.kt
@@ -0,0 +1,40 @@
+package com.cheze.upapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.cheze.upapp.model.Account
+
+class MyBankAccountRecyclerViewAdapter(
+    private val values: List<Account>
+) : RecyclerView.Adapter<MyBankAccountRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        with (item) {
+            with (attr) {
+                holder.idView.text = name
+                holder.contentView.text = "${balance.currency} ${balance.value}"
+            }
+        }
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val idView: TextView = view.findViewById(R.id.item_number)
+        val contentView: TextView = view.findViewById(R.id.content)
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+}
